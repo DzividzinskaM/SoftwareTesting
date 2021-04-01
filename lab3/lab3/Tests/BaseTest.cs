b9feb872658da4951885732975cda82b14966e0c
DzividzinskaM <marianadzividzinska04@gmail.com>
2021-04-01 10:23:13 +0300

    fix: change driver

--- lab3/lab3/Tests/BaseTest.cs
+++ lab3/lab3/Tests/BaseTest.cs
@@ -1,6 +1,6 @@
 ﻿
 using NUnit.Framework;
-using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace lab3.Tests
 {
@@ -14,8 +14,8 @@ namespace lab3.Tests
         public void beforeClass()
         {
 
-
-            Driver = new ChromeDriver("D:/Fork/qa-course/lab3/resources");
+            Driver = new FirefoxDriver();
+            /*Driver = new ChromeDriver("D:/Fork/qa-course/lab3/resources");*/
 
         }
 
